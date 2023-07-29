8e867ab93fa46049726c73234b33306cda51e891
David Morton <github@davidmorton.io>
2023-07-29 07:13:48 -0500

    Refined

--- patch_cirrus/patch_cirrus.c
+++ patch_cirrus/patch_cirrus.c
@@ -976,10 +976,11 @@ static int patch_cs4208(struct hda_codec *codec)
 	spec->gen.automute_hook = cs_automute;
 	/* exclude NID 0x10 (HP) from output volumes due to different steps */
 	spec->gen.out_vol_mask = 1ULL << 0x10;
-	spec->gen.autocfg.speaker_outs[0] = 0x11;
-	spec->gen.autocfg.speaker_outs[1] = 0x12;
-	spec->gen.autocfg.speaker_outs[2] = 0x13;
-	spec->gen.autocfg.speaker_outs[3] = 0x14;
+	spec->gen.autocfg.speaker_outs = 4;
+	spec->gen.autocfg.speaker_pins[0] = 0x11;
+	spec->gen.autocfg.speaker_pins[1] = 0x12;
+	spec->gen.autocfg.speaker_pins[2] = 0x13;
+	spec->gen.autocfg.speaker_pins[3] = 0x14;
 
 	snd_hda_pick_fixup(codec, cs4208_models, cs4208_fixup_tbl,
 			   cs4208_fixups);
